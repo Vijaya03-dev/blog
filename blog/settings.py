a727a90b56e1e1a105d0d845d2e21a1b264ce37b
vijaya <vijayaprajapati03@gmail.com>
2026-04-05 23:21:51 +0530

    latest commit after db

--- blog/settings.py
+++ blog/settings.py
@@ -82,11 +82,7 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True
-    )
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 
 # Password validation
